1202e449ce5a9cf40f032e793cf2687eab90ccd9
iamtobbi <adenijiadetayo92@gmail.com>
2023-03-21 19:11:06 -0700

    times table

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,14 @@
-nclude "main.h"
+#include "main.h"
 
 
 /**
- * print_sign - prints the sign of a numberl
+ * print_sign - prints the sign of a number
+ *
  * @n: the int to check
+ *
  * Return: 1 and prints + if n is greater than zero
  *
- * 0 and prints 0 if n == zero
+ * 0 and prints 0 if n is zero
  *
  * -1 and prints - if n is less than zero
  */
@@ -23,7 +25,9 @@ int print_sign(int n)
 
 		return (1);
 
-	} else if (n == 0)
+	}
+       
+	else if (n == 0)
 
 }
 
@@ -32,12 +36,15 @@ _putchar(48);
 return (0);
 
 }
+
 else if (n < 0)
 
 {
 
 	_putchar('-');
+
 }
 
 return (-1);
+
 }
